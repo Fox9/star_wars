2f80b219fcfaf527ec45930758e283f2511ef1a3
Ellan Esenaliev <ellanesenaliev@MacBook-Pro-Ellan.local>
2019-06-03 21:37:37 +0600

    Init

---
+++ TheStarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheStarWars
+//
+//  Created by Ellan Esenaliev on 6/3/19.
+//  Copyright © 2019 Ellan Esenaliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
